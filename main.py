5b5662422f3865c10b560d0c74b78eb5582e29b5
vmedrano14c <141966048+vmedrano14c@users.noreply.github.com>
2023-08-22 04:20:17 -0500

    Prueba 1
    Hice la correción del código e intento saber si esta bien

--- main.py
+++ main.py
@@ -1,48 +1,47 @@
 class Asiento:
-    def init(self,color,precio,registro):
+    def _init_(self, color, precio, registro):
         self.color= color
         self.precio= precio
         self.registro = registro
     def cambiarColor(self,color):
-        color=str(input())
         if (color == "rojo" or color=="verde" or color=="amarillo" or color=="negro" or color=="blanco"):
             self.color=color
             return self.color
 
 class Motor:
-    def init(self,numeroCilindros,tipo,registro):
+    def _init_(self, numeroCilindros, tipo, registro):
         self.numeroCilindros = numeroCilindros
         self.tipo= tipo
         self.registro= registro
     def cambiarRegistro(self,registro):
         self.registro= registro
         return self.registro
+
     def asignarTipo(self,tipo):
         if(tipo=="electrico" or tipo=="gasolina"):
             self.tipo = tipo
-            return self.tipo
-        
+        return self.tipo
 class Auto:
     cantidadCreados=0
-    def _init_(self,modelo,precio,asientos,marca,motor,registro):
+    def _init_(self, modelo, precio, asientos, marca, motor, registro):
         self.modelo = modelo
         self.precio= precio
-        self.asientos = []
+        self.asientos = asientos
         self.marca = marca
-        self.motor = Motor
+        self.motor = motor
         self.registro = registro
     def cantidadAsientos(self):
         contarAsientos=0
-        for i in range(len(asientos)):
-            if(asientos[i]!=None):
+        for asiento in self.asientos:
+            if(asiento!=None):
                 contarAsientos+=1
         return contarAsientos
     def verificarIntegridad(self):
-        if(registro==motor.registro):
-            for i in range(len(asientos)):
-               if(asientos[i]!=None):
-                    if(asientos[i].registro!=registro):
-                        return print("Las piezas no son originales") 
-            return print("Auto original")
+        if(self.registro==self.motor.registro):
+            for i in self.asientos:
+               if(i!=None):
+                    if(i.registro!=self.registro):
+                        return "Las piezas no son originales" 
+            return "Auto original"
         else:
-            return print("Las piezas no son originales")
+            return "Las piezas no son originales"
